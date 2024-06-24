8ff4c1cc9ebe436a8a08367f681dc0a7d5e9ea61
Barron Vistan <barronv@careindeed.com>
2024-06-24 03:40:48 -0700

    add dark mode

--- ci-page/src/components/Header.js
+++ ci-page/src/components/Header.js
@@ -3,6 +3,7 @@ import MenuItem from './MenuItem'
 import {AiFillHome} from 'react-icons/ai';
 import {BsFillInfoCircleFill } from 'react-icons/bs'
 import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
 
 export default function Header() {
   return (
@@ -11,10 +12,14 @@ export default function Header() {
         <MenuItem title="home" address="/" Icon={AiFillHome}/>
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill}/>
       </div>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch/>
       <Link href={'/'} className='flex gap-1 items-center'>
         <span className='text-2xl font-bold bg-teal-700 py-1 px-2 rounded-lg text-white'>CI</span>
         <span className='text-xl hidden sm:inline'>Inquiry</span>
       </Link>
+      </div>
+     
     </div>
   )
 }
