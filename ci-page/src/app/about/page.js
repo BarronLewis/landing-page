f6c9dd4a8c3337f7859a998c18e1d1e4c226a5ed
Barron Vistan <barronv@careindeed.com>
2024-06-24 05:03:24 -0700

    add form

---
+++ ci-page/src/app/about/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-orange-600'>About</h1>
+      <p>This a sample landing page for the CI website for my coding assessment.</p>
+    </div>
+  )
+}
--- ci-page/src/app/page.js
+++ ci-page/src/app/page.js
@@ -3,8 +3,22 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-orange-600'>Inquiry Form</h1>
+      <form className='grid grid-cols-1'>
+        <label className='flex gap-4'>Name: <input type="text" name="name" className='border-2 border-black px-2 rounded-md'/></label>
+        <label className='flex gap-4'>Email: <input type="email" name="email" className='border-2 border-black px-2 rounded-md'/></label>
+        <label className='flex gap-4'>Zip Code: <input type="text" name="zipcode" className='border-2 border-black px-2 rounded-md'/></label>
+        <label className='flex gap-4'>Service: 
+        <select className='border-2 border-black px-2 rounded-md'>
+          <option value="Assisted Living">Assisted Living</option>
+          <option value="Home Care">Home Care</option>
+          <option value="Home Health">Home Health</option>
+          <option value="Independent Living">Independent Living</option>
+        </select>
+        </label>
+        <button type="submit" className='max-w-24 bg-slate-400 rounded-lg'>Submit</button>
+      </form>
     </div>
   )
 }
